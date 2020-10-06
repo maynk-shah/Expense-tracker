071d3d130562848dc9b06be1261382708a4090df
MAYANKshahh <shahmayank.shah48@gmail.com>
2020-10-06 13:36:35 -0400

    first

---
+++ src/components/TransactionList.js
@@ -0,0 +1,28 @@
+import React, { useContext, useEffect } from "react";
+import { Transaction } from "./Transaction";
+
+import { GlobalContext } from "../context/GlobalState";
+
+export const TransactionList = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  useEffect(() => {
+    localStorage.setItem("Texts", JSON.stringify(transactions));
+    // localStorage.setItem("Amount");
+  }, [transactions]);
+
+  // console.log(JSON.stringify(transactions));
+  return (
+    <>
+      <h3>History</h3>
+      <ul className="list">
+        {transactions.map((transaction) => (
+          <Transaction
+            key={transaction.id}
+            transaction={transactions}
+          />
+        ))}
+      </ul>
+    </>
+  );
+};
